13345e42813a4a553130b8bb5b7fc6336ff3b8bc
BlertaKryeziu <bk52307@ubt-uni.net>
2023-01-26 02:38:34 +0100

    changes

---
+++ OnlineShop/Utility/SessionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Newtonsoft.Json;
+
+namespace OnlineShop.Utility
+{
+    public static class SessionExtensions
+    {
+        public static void Set<T>(this ISession session, string key, T valvue)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(valvue));
+        }
+
+        public static T Get<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T) :
+                JsonConvert.DeserializeObject<T>(value);
+
+        }
+
+    }
+}
